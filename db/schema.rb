1e8de33e20f208f8f74d33ec7be84d8e68845de4
Manny Ram <manrm8@gmail.comgit config --global user.push.default matchinggit initgit add .git statusgit config --global user.email manrm8@gmail.com>
2015-06-08 23:06:00 +0000

    Added association for plans and users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150608224630) do
+ActiveRecord::Schema.define(version: 20150608230307) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(version: 20150608224630) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "plan_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
